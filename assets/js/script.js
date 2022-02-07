924fb9ffe48b48d950c721525ea926c1e48d589b
christianvegag <christianvegaguizabalo@gmail.com>
2022-02-07 02:53:13 -0500

    Creacion de header con boostrap y css nuevo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,9 @@
-
-console.log("Example");
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
