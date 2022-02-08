cc614bf3352a20675f6803b80ece99583cbbb062
sedian27 <diestrasergio@gmail.com>
2022-02-07 19:12:36 -0500

    Mejoras y pasando codigo css a bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
 $(function () {
-    $(window).on('scroll', function () {
-        if ( $(window).scrollTop() > 10 ) {
-            $('.navbar').addClass('active');
-        } else {
-            $('.navbar').removeClass('active');
-        }
-    });
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 10) {
+      $(".navbar").addClass("active");
+    } else {
+      $(".navbar").removeClass("active");
+    }
+  });
 });
-=======
-
->>>>>>> body_article
